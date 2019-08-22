0f5898392f85aeaa1a4f514427657b7cf543542d
Ayush <u6012645@anu.edu.au>
2019-08-22 11:32:03 +0530

    dev

--- src/insert.py
+++ src/insert.py
@@ -1,17 +1,26 @@
 #####
 #The functions here are used to insert a list of elements in a file after a label. 
 #####
+
+def insert_elements_dev(file,elements,label):
+    elements = [line + "\n" for line in elements]
+    fo = open(file, "r+")
+    content = fo.read()
+    fo.seek(content.index(label)+len(label))
+    fo.writelines(["\n"] +elements)
+    fo.close()
+
+
 def insert_elements(file,elements,label): #inserts elements in a file after a label
     fo = open(file, "r+")
-    content = [x.strip() for x in fo] 
+    content = [line.strip() for line in fo] 
     new_content = content[0:content.index(label)+1]
     new_content = new_content + elements + content[content.index(label)+1:]
-    final = []
-    for x in new_content:
-        final.append(x + "\n")
+    new_content = [line + "\n" for line in new_content]
     fo.close()
     with open(file, 'w+') as fp:
-        fp.writelines(final)
+        fp.writelines(new_content)
+    fp.close()
 
 def insert_file(file1,file2,label): #inserts elements of a file2 in file1 after a label. 
     fo = open(file2, "r")
