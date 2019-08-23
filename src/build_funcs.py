c7da4b775cf2aae1b478fd348048b157b3162177
Ayush <u6012645@anu.edu.au>
2019-08-23 11:25:27 +0530

    easier page naming

--- src/build_funcs.py
+++ src/build_funcs.py
@@ -6,29 +6,32 @@ import json
 
 def build_website(data):
     os.system("cp -r assets/themes/"+ data["settings"]["theme"]+" template")
-    names = data["settings"]["pages"]
+    settings = data["settings"]
+    page_names = settings["pages"]
+    page_dict = {}
+    for x in page_names:page_dict[x] = x.replace(' ','_')
     home = "template/index.html"
-    Author_image='"'+data["settings"]["image"] +'"'
+    Author_image='"'+settings["image"] +'"'
     insert_replace(home,Author_image,'"fakelink"')
-    copy_between(home,'<!-- Page list start-->','<!-- Page list end-->',len(names)-1) 
-    for x in names:
-        page_link = "template/"+list(x.values())[0]+".html"
+    copy_between(home,'<!-- Page list start-->','<!-- Page list end-->',len(page_names)-1) 
+    for x in page_names:
+        page_link = "template/"+page_dict[x]+".html"
         os.system("cp template/left-sidebar.html "+page_link)
         insert_replace(home,page_link[9:],'"sameplepage.html"')
-        insert_replace(home,list(x.keys())[0],'"samplepage"')
-        insert_replace(page_link,list(x.keys())[0],'"sampletitle"')
-        insert_replace(page_link,list(x.keys())[0],'"nameofauthor"')
-        insert_elements(page_link,ordering(list(x.values())[0],data),'<!-- Content Generator -->')
+        insert_replace(home,x,'"samplepage"')
+        insert_replace(page_link,x,'"sampletitle"')
+        insert_replace(page_link,x,'"nameofauthor"')
+        insert_elements(page_link,ordering(page_dict[x],data),'<!-- Content Generator -->')
         insert_replace(page_link,Author_image,'"fakelink"')
 
-    insert_replace(home,data["settings"]["name"],'"nameofauthor"')
+    insert_replace(home,settings["name"],'"nameofauthor"')
     Author_details = ordering("Home",data)
     insert_elements(home,Author_details,'<!--About me -->')
-    insert_replace(home,data["settings"]["email"],'"example@gmail.com"')
-    if data["settings"]["support_us"]=="YES":
+    insert_replace(home,settings["email"],'"example@gmail.com"')
+    if settings["support_us"]=="YES":
         insert_file(home,"template/ad.html","<!--Footer -->")
-    for x in names:
-        activator(list(x.values())[0],data) #REACTIVE TITLE BAR 
+    for x in page_names:
+        activator(page_dict[x],data) #REACTIVE TITLE BAR 
 
 
 
