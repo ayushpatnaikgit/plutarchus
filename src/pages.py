01b6af365958495e0703dcdc4b7b8d743d6a2d38
Ayush <ayushpatnaik@gmail.com>
2019-08-24 16:42:01 +0530

    easier json file and working hjson file

--- src/pages.py
+++ src/pages.py
@@ -16,24 +16,30 @@ def subsection(B):
 def entry(ENTRY):
     return "<p>" + ENTRY + "</p>" 
 
+def check_empty(dict,key):
+    try:
+        value = dict[key]
+    except:
+        value = ""
+    return value 
+
 def ordering(page,data): #This is the most important part of the code. Iterating through sections and subsecitons to add entries in proper order in HTML format
     pagedictionary = pageDict(page,data)
-    #print(pagedictionary)
     html_page = []
     section_list = []
     for x in pagedictionary: 
         subsection_list = []
-        if x["section"] not in section_list: 
-            section_list.append(x["section"])
-            html_page.append(section(x["section"]))
+        if check_empty(x,"section") not in section_list: 
+            section_list.append(check_empty(x,"section"))
+            html_page.append(section(check_empty(x,"section")))
             for y in pagedictionary:
-                if y["section"] == x["section"]:
-                    if y["subsection"] not in subsection_list:
-                        subsection_list.append(y["subsection"])
-                        html_page.append(subsection(y["subsection"]))
+                if check_empty(y,"section") == check_empty(x,"section"):
+                    if check_empty(y,"subsection") not in subsection_list:
+                        subsection_list.append(check_empty(y,"subsection"))
+                        html_page.append(subsection(check_empty(y,"subsection")))
                         entries = []
                         for z in pagedictionary: 
-                            if z["section"] == x["section"] and z["subsection"] == y["subsection"]:
+                            if check_empty(z,"section") == check_empty(x,"section") and check_empty(z,"subsection") == check_empty(y,"subsection"):
                                 entries.append(entry(z["entry"])) 
                         try: #try sorting if dates are there
                             entries = sorted(entries, key = lambda i:  datetime.datetime.strptime(i['date'], '%d/%m/%Y'))[::-1]
--- src/plutarchus.py
+++ src/plutarchus.py
@@ -5,9 +5,16 @@ from pages import *
 import json
 import sys
 from build_funcs import *
-# reading csv file 
-with open(sys.argv[1]) as json_file:  
-    data_file = json.load(json_file)
+file = sys.argv[1]
+if '.hjson' in file:
+    os.system("rm -rf temp_json_from_hjson.json")
+    os.system("hjson -j "+sys.argv[1]+" >> temp_json_from_hjson.json") 
+    with open("temp_json_from_hjson.json") as json_file:  
+        data_file = json.load(json_file)
+        os.system("rm -rf temp_json_from_hjson")
+else: 
+    with open(file) as json_file:  
+        data_file = json.load(json_file)
 #creating files using headinds
 os.system("rm -rf template")
 build_website(data_file)
